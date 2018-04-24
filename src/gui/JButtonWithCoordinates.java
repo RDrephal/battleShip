2cf8aa4938eef2997c8147a928c3afe8f5c12bc7
Philipp <phildersiebers@gmx.de>
2018-04-24 20:20:54 +0200

    Nothing

--- src/gui/JButtonWithCoordinates.java
+++ src/gui/JButtonWithCoordinates.java
@@ -1,4 +1,15 @@
 package src.GUI;
 
-public class JButtonWithCoordinates {
+import javax.swing.*;
+
+public class JButtonWithCoordinates extends JButton {
+    int xValue;
+    int yValue;
+
+    public JButtonWithCoordinates(int xValue, int yValue){
+        this.xValue = xValue;
+        this.yValue = yValue;
+
+        this.setText("EigenerButton");
+    }
 }
