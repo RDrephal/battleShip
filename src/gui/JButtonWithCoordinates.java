bd0e579702af7d4384eb481702b3b640ffcc1f9f
Philipp <phildersiebers@gmx.de>
2018-04-28 15:30:59 +0200

    Buttons accessible by coordinate key

--- src/gui/JButtonWithCoordinates.java
+++ src/gui/JButtonWithCoordinates.java
@@ -1,25 +1,25 @@
 package src.GUI;
 
+import model.Coordinates;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class JButtonWithCoordinates extends JButton {
-    private int xValue;
-    private int yValue;
+    private model.Coordinates coords;
     private Font font;
     private Color defaultColor;
 
 
-    public JButtonWithCoordinates(int xValue, int yValue){
+    public JButtonWithCoordinates(model.Coordinates coords){
         font = new Font("Arial", Font.BOLD, 20);
         this.setFont(font);
 
         this.defaultColor = new Color(0,102,153);
 
-        this.xValue = xValue;
-        this.yValue = yValue;
+        this.coords = coords;
 
-        this.setText("");
+        resetButton();
     }
 
     public void resetButton(){
@@ -27,10 +27,12 @@ public class JButtonWithCoordinates extends JButton {
         this.setBackground(defaultColor);
     }
 
+    //Change button to indicate a hit
     public void hit(){
         this.setText("X");
     }
 
+    //Change button to indicate a miss
     public void noHit(){
         this.setText("O");
     }
@@ -39,11 +41,15 @@ public class JButtonWithCoordinates extends JButton {
         //Um mehrere Felder farbig zu hinterlegen
     }
 
-    public int getXValue(){
-        return xValue;
+    public String getXValue(){
+        return coords.getX();
     }
 
     public int getYValue(){
-        return yValue;
+        return coords.getY();
+    }
+
+    public Coordinates getCoords() {
+        return coords;
     }
 }
