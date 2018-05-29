5d53cc22da7b771123075ca90aa7936b1b5c2979
RDrephal <robin.drephal@gmail.com>
2018-05-29 20:34:58 +0200

    [add] Requesthandler and StreamHandler

---
+++ src/main/java/alexa/requesthandler/LaunchRequestHandler.java
@@ -0,0 +1,29 @@
+package alexa.requesthandler;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
+
+import java.util.Optional;
+
+
+public class LaunchRequestHandler  implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(Predicates.requestType(LaunchRequest.class));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+        String speechText = "Hello Player, shot on target";
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
+}
---
+++ src/main/java/alexa/steamhandler/AlexaBattleShipStreamHandler.java
@@ -0,0 +1,22 @@
+package alexa.steamhandler;
+
+import alexa.requesthandler.CancelandStopIntentHandler;
+import alexa.requesthandler.HelpIntentHandler;
+import alexa.requesthandler.LaunchRequestHandler;
+import alexa.requesthandler.SessionEndedRequestHandler;
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+
+public class AlexaBattleShipStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(new CancelandStopIntentHandler(),  new HelpIntentHandler(), new LaunchRequestHandler(), new SessionEndedRequestHandler())
+                .build();
+    }
+
+    public AlexaBattleShipStreamHandler() {
+        super(getSkill());
+    }
+}
