b6c217c923880f0cd1ea833a708e23f4f84c6e03
RDrephal <robin.drephal@gmail.com>
2018-07-17 12:04:40 +0200

    add sleep

--- src/main/java/battleship/helper/AlexaResponseHelper.java
+++ src/main/java/battleship/helper/AlexaResponseHelper.java
@@ -5,6 +5,8 @@ import battleship.model.ShotEvent;
 
 import java.lang.reflect.Array;
 
+import static java.lang.Thread.sleep;
+
 public class AlexaResponseHelper {
 
     public static String[] getShotResponse(JButtonWithCoordinates jb, String playerEvent) {
@@ -14,6 +16,11 @@ public class AlexaResponseHelper {
         } else {
             if (jb.getState() == ShotEvent.WAIT) {
                 jb.doClick();
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 playerEvent = Helper.getCurrentPlayerEvent().toString();
                 computerEvent = Helper.getCurrentComputerEvent().toString();
             } else {
